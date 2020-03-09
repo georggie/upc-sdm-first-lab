b5592f0eab8ed41aa64f18081f82f6f77edc1556
georggie <krivokapic.djordjije@gmail.com>
2020-03-09 13:13:13 +0100

    loading citations

--- database/neo4j_loader.py
+++ database/neo4j_loader.py
@@ -19,6 +19,7 @@ class Neo4JLoader(object):
         """
         self._load_journals()
         self._load_conferences_and_workshops()
+        self._generate_random_citations()
 
     def _load_journals(self):
         """
@@ -30,15 +31,15 @@ class Neo4JLoader(object):
             session = self._driver.session()
             session.run("""
                 LOAD CSV WITH HEADERS FROM 'file:///journals.csv' AS row
-                MERGE (paper:Paper {id: row.key, title: row.title, abstract: row.abstract, 
-                       pages: coalesce(row.pages, 38-40), url: coalesce(row.ee, 'Unknown')})
+                MERGE (paper:ScientificPaper {key: row.key, title: row.title, abstract: row.abstract, 
+                       pages: coalesce(row.pages, 38-40), url: row.ee})
                 MERGE (author:Author {name: row.author})
                 MERGE (author)-[:IS_LEAD_AUTHOR]->(paper)
                 WITH row, author, paper
                 WHERE row.coauthors IS NOT NULL
                 UNWIND split(row.coauthors, '|') AS co_author
                 MERGE (coauthor:Author {name: co_author})
-                MERGE (paper)<-[:WRITES]-(coauthor)
+                MERGE (paper)<-[:IS_CO_AUTHOR]-(coauthor)
                 WITH row, paper
                 UNWIND split(row.keywords, '|') AS keywrd
                 MERGE (keyword:Keyword {name: keywrd})
@@ -47,9 +48,9 @@ class Neo4JLoader(object):
                 MERGE (journal:Journal {name: row.journal})
                 MERGE (paper)-[:PUBLISHED_IN]->(journal)
                 WITH row, journal
-                MERGE (volume:Volume {edition: row.volume})
-                MERGE (date:Date {year: row.year})
-                MERGE (journal)-[:BELONGS_TO]->(volume)-[:PUBLISHED]->(date)
+                MERGE (volume:Volume {number: row.volume})
+                MERGE (date:Year {year: row.year})
+                MERGE (journal)-[:BELONGS_TO]->(volume)-[:ISSUED]->(date)
             """)
         except Exception as exception:
             print("Exception => ", exception)
@@ -59,40 +60,78 @@ class Neo4JLoader(object):
         Load conferences and journals to the graph database
         :return: void
         """
-        print('Loading conferences to neo4j ...')
         try:
+            print('Loading conferences to neo4j ...')
             session = self._driver.session()
             session.run("""
                 LOAD CSV WITH HEADERS FROM 'file:///conferences.csv' AS row
-                MERGE (paper:Paper {id: row.key_x, title: row.title_x, abstract: row.abstract, 
-                       pages: coalesce(row.pages_x, 38-40), url: coalesce(row.ee_x, 'Unknown')})
+                MERGE (paper:ScientificPaper {id: row.key_x, title: row.title_x, abstract: row.abstract, 
+                       pages: row.pages_x, url: row.ee_x})
                 MERGE (author:Author {name: row.author_x})
                 MERGE (author)-[:IS_LEAD_AUTHOR]->(paper)
-                WITH row, author, paper
+                WITH  row, author, paper
                 WHERE row.coauthors IS NOT NULL
                 UNWIND split(row.coauthors, '|') AS co_author
                 MERGE (coauthor:Author {name: co_author})
-                MERGE (paper)<-[:WRITES]-(coauthor)
-                WITH row, paper
+                MERGE (paper)<-[:IS_CO_AUTHOR]-(coauthor)
+                WITH  row, paper
                 UNWIND split(row.keywords, '|') AS keywrd
                 MERGE (keyword:Keyword {name: keywrd})
                 MERGE (paper)-[:MENTIONES]->(keyword)
-                WITH row, paper
-                MERGE (proceeding:Proceeding {id: row.key_y, isbn: coalesce(row.isbn, 'Unknown'), url: coalesce(row.ee_y, 'Unknown'), 
-                       published: row.publisher, series: coalesce(row.series, 'Unknown')})
+                WITH  row, paper
+                MERGE (proceeding:Proceeding {key: row.key_y, isbn: row.isbn, url: row.ee_y, 
+                       published: row.publisher, series: row.series})
                 MERGE (paper)-[:IS_IN]->(proceeding)
-                WITH row, SPLIT(row.title_y, "|") AS words, proceeding
-                WHERE words[5] = 'False'
-                MERGE (conf:Conference{name: words[0]})<-[:OF_A]-(proceeding)
-                MERGE (conf)-[:HAS]->(edition:Edition {place: words[1], country: words[2], period: words[3]})
-                MERGE (edition)-[:HAPPENED]-(date: Date {year: words[4]})
-                WITH SPLIT(row.title_y, "|") AS words, proceeding
-                WHERE words[5] = 'True'
-                MERGE (work:Workshop{name: words[0]})<-[:OF_A]-(proceeding)
-                MERGE (work)-[:HAS]->(edition:Edition {place: words[1], country: words[2], period: words[3]})
-                MERGE (edition)-[:HAPPENED]-(date: Date {year: words[4]})
+                WITH  row, proceeding
+                WHERE SPLIT(row.title_y, "|")[5] = 'False'
+                MERGE (conf:Conference{name: SPLIT(row.title_y, "|")[0]})<-[:OF_A]-(proceeding)
+                MERGE (conf)-[:HAS]->(edition:Edition {city: SPLIT(row.title_y, "|")[1], country: SPLIT(row.title_y, "|")[2], period: SPLIT(row.title_y, "|")[3]})
+                MERGE (edition)-[:HAPPENED]-(date: Year {year: SPLIT(row.title_y, "|")[4]})
+            """)
+
+            print('Loading workshops to neo4j ...')
+            session = self._driver.session()
+
+            session.run("""
+                LOAD CSV WITH HEADERS FROM 'file:///conferences.csv' AS row
+                MERGE (paper:ScientificPaper {key: row.key_x, title: row.title_x, abstract: row.abstract, 
+                       pages: row.pages_x, url: row.ee_x})
+                MERGE (author:Author {name: row.author_x})
+                MERGE (author)-[:IS_LEAD_AUTHOR]->(paper)
+                WITH  row, author, paper
+                WHERE row.coauthors IS NOT NULL
+                UNWIND split(row.coauthors, '|') AS co_author
+                MERGE (coauthor:Author {name: co_author})
+                MERGE (paper)<-[:IS_CO_AUTHOR]-(coauthor)
+                WITH  row, paper
+                UNWIND split(row.keywords, '|') AS keywrd
+                MERGE (keyword:Keyword {name: keywrd})
+                MERGE (paper)-[:MENTIONES]->(keyword)
+                WITH  row, paper
+                MERGE (proceeding:Proceeding {key: row.key_y, isbn: row.isbn, url: row.ee_y, published: row.publisher, series: row.series})
+                MERGE (paper)-[:IS_IN]->(proceeding)
+                WITH  row, proceeding
+                WHERE SPLIT(row.title_y, "|")[5] = 'True'
+                MERGE (conf:Workshop{name: SPLIT(row.title_y, "|")[0]})<-[:OF_A]-(proceeding)
+                MERGE (conf)-[:HAS]->(edition:Edition {city: SPLIT(row.title_y, "|")[1], country: SPLIT(row.title_y, "|")[2], period: SPLIT(row.title_y, "|")[3]})
+                MERGE (edition)-[:HAPPENED]-(date: Year {year: SPLIT(row.title_y, "|")[4]})
             """)
         except Exception as exception:
             print("Exception => ", exception)
+
+    def _generate_random_citations(self):
+        """
+        Generate random citations between scientific papers
+        :return: void
+        """
+        print('Generating random citations between papers ...')
+        session = self._driver.session()
+
+        session.run("""
+            MATCH (scp1:ScientificPaper), (scp2:ScientificPaper)
+            WITH scp1, scp2
+            WHERE rand() < 0.1 AND scp1 <> scp2
+            MERGE (scp1)-[:CITES]->(scp2)
+        """)
 
 
